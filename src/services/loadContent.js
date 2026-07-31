9bc88d3b4328d58641b13decad9f0ddc4226757e
planephoto88-crypto <planephoto88@gmail.com>
2026-07-31 12:33:33 +0545

    Update all changes

--- src/services/loadContent.js
+++ src/services/loadContent.js
@@ -1,7 +1,23 @@
 
 import { API_BASE } from "../constants/api";
 
+const FALLBACK_BASE = "http://localhost:5000";
+
 const cache = {};
+
+async function fetchContent(base, subject, chapter, topic) {
+  const response = await fetch(
+    `${base}/api/content/${subject}/${chapter}/${topic}`
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to load content: ${subject}/${chapter}/${topic}`
+    );
+  }
+
+  return response.json();
+}
 
 export async function loadContent(subject, chapter, topic) {
   const key = `${subject}-${chapter}-${topic}`;
@@ -10,17 +26,17 @@ export async function loadContent(subject, chapter, topic) {
     return cache[key];
   }
 
-  const response = await fetch(
-  `${API_BASE}/api/content/${subject}/${chapter}/${topic}`
-);
+  let data;
 
-  if (!response.ok) {
-    throw new Error(
-      `Failed to load content: ${subject}/${chapter}/${topic}`
-    );
+  try {
+    data = await fetchContent(API_BASE, subject, chapter, topic);
+  } catch (err) {
+    if (API_BASE === FALLBACK_BASE) {
+      throw err;
+    }
+
+    data = await fetchContent(FALLBACK_BASE, subject, chapter, topic);
   }
-
-  const data = await response.json();
 
   cache[key] = data;
 
